das7pad/metrics-sharelatex

[misc] register the metrics sweeping interval handle for later cleanup

index.js
@@ -29,6 +29,7 @@ function configure(opts = {}) {
 function initialize(_name, opts = {}) {
   configure({ ...opts, appName: _name })
   collectDefaultMetrics({ timeout: 5000, prefix: '' })
+  prom.setupSweeping()
 
   console.log(`ENABLE_TRACE_AGENT set to ${process.env.ENABLE_TRACE_AGENT}`)
   if (process.env.ENABLE_TRACE_AGENT === 'true') {

overleaf/metrics-module

index.js
@@ -30,6 +30,7 @@ function initialize(appName, opts = {}) {
   appName = appName || DEFAULT_APP_NAME
   configure({ ...opts, appName })
   collectDefaultMetrics({ timeout: 5000, prefix: '' })
+  promWrapper.setupSweeping()
 
   console.log(`ENABLE_TRACE_AGENT set to ${process.env.ENABLE_TRACE_AGENT}`)
   if (process.env.ENABLE_TRACE_AGENT === 'true') {